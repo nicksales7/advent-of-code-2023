73c5fdebb690d3c4e0aa043b8d3d5714561baad3
nicksales7 <nicksales72@gmail.com>
2023-12-03 17:55:15 -0500

    got this first try lol

--- day2/part2.py
+++ day2/part2.py
@@ -17,12 +17,36 @@ with open("input.txt", "r") as file:
     data = file.read()
     games_list = set_number(data)
     
-    possible_games_sum = 0
-
+    max_red = 0
+    max_green = 0
+    max_blue = 0
+    
+    power = 0
     for game_index in range(len(games_list)):
+        for round in games_list[game_index]:
+            for color_count in round:
+                number, color = color_count.split()
+                number = int(number)
+
+                if color == "red" and number > max_red:
+                    max_red = number
+                elif color == "green" and number > max_green:
+                    max_green = number
+                elif color == "blue" and number > max_blue:
+                    max_blue = number
+        power += max_red * max_green * max_blue
+        max_red = 0
+        max_green = 0
+        max_blue = 0
+            
+
+    print(power)
+    
+    '''
+    for game_index in range(len(lst)):
         game_possible = True
 
-        for round in games_list[game_index]:
+        for round in lst[game_index]:
             for color_count in round:
                 number, color = color_count.split()
                 number = int(number)
@@ -38,3 +62,4 @@ with open("input.txt", "r") as file:
             possible_games_sum += game_index + 1
 
     print(possible_games_sum)
+    '''

fda8e896cd36c55c2f6157240e44fae84921af35
nicksales7 <nicksales72@gmail.com>
2023-12-03 17:56:00 -0500

    got this first try lol

--- day2/part2.py
+++ day2/part2.py
@@ -41,25 +41,4 @@ with open("input.txt", "r") as file:
             
 
     print(power)
-    
-    '''
-    for game_index in range(len(lst)):
-        game_possible = True
-
-        for round in lst[game_index]:
-            for color_count in round:
-                number, color = color_count.split()
-                number = int(number)
-
-                if (color == "red" and number > 12) or (color == "green" and number > 13) or (color == "blue" and number > 14):
-                    game_possible = False
-                    break  
-
-            if not game_possible:
-                break 
-
-        if game_possible: 
-            possible_games_sum += game_index + 1
-
-    print(possible_games_sum)
-    '''
+   
